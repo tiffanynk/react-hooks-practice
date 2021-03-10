cede1d6f3521a68108b2a3711d8d6ed86730c60a
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2021-03-09 21:45:05 -0700

    class component counter with autocount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    count: 0
+  };
+
+  componentDidMount = () => {
+    setInterval(() => this.incrementCount(), 1000)
+  };
+
+  decrementCount = () => {
+    this.setState({count: this.state.count - 1})
+  };
+
+  incrementCount = () => {
+    this.setState({count: this.state.count + 1})
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.count}</p>
+        <button onClick={this.decrementCount}>-</button>
+        <button onClick={this.incrementCount}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
